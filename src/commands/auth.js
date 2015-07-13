10a7d4f6fadcadb915eeda2ea100b4e2fdaeaba9
M4GNV5 <eragonjml@googlemail.com>
2015-07-13 23:36:56 +0200

    updated auth.js

--- src/commands/auth.js
+++ src/commands/auth.js
@@ -7,11 +7,19 @@ var invitedUsers = [];
 var init = function(bot)
 {
 	bot.permLevel = {};
+
+	bot.on("onlineRemove", function(data)
+	{
+		if(typeof bot.permLevel[data.nick] != 'undefined')
+		{
+			delete bot.permLevel[data.nick];
+		}
+	});
 }
 
 var login = function(bot, sender, args)
 {
-	lib.awaitPrivateInput(bot, sender, 60000, function(_bot, joined)
+	lib.awaitPrivateInput(bot, sender, 180000, function(_bot, joined)
 	{
 		_bot.send("Awaiting password for " + sender + " in ?" + bot.channel);
 	}, function(_bot, _sender, text)
@@ -21,7 +29,6 @@ var login = function(bot, sender, args)
 		if(typeof knowUsers[pw] != 'undefined' && knowUsers.hasOwnProperty(pw))
 		{
 			var user = knowUsers[pw];
-			_bot.send("@" + _sender + " successfully authed as " + user.nick + " in ?" + bot.channel)
 			bot.send("@" + sender + " successfully authed as " + user.nick);
 			bot.permLevel[sender] = user.level;
 
@@ -31,17 +38,4 @@ var login = function(bot, sender, args)
 	});
 };
 
-var logout = function(bot, sender, args)
-{
-	if(typeof bot.permLevel[sender] == 'number')
-	{
-		delete bot.permLevel[sender];
-		bot.send("@" + sender + " logout successfull");
-	}
-	else
-	{
-		bot.send("@" + sender + " you are not logged in");
-	}
-}
-
-module.exports = { init:init, login: login, logout: logout };
+module.exports = { init:init, login: login };
