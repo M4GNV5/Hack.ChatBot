cc1b41278977ed7798c273ba0fdd57699c9e0f95
M4GNV5 <eragonjml@googlemail.com>
2015-07-14 17:32:20 +0200

    ascii command now requires a permLevel of 1

--- src/commands/ascii.js
+++ src/commands/ascii.js
@@ -2,6 +2,12 @@ var ascii = require("figlet");
 
 var createAsciiArt = function(bot, sender, args)
 {
+	if(typeof bot.permLevel[sender] == 'undefined' || bot.permLevel[sender] < 1)
+	{
+		bot.send("@" + sender + " you dont have the permission to use this command");
+		return;
+	}
+
 	var text = args.join(" ");
 
 	ascii(text, function(err, data)
