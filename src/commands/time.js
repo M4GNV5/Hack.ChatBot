a8d54516b32ac029213b174b59c71a645162d8f1
M4GNV5 <eragonjml@googlemail.com>
2015-07-18 19:10:11 +0200

    added searching by city to time

--- src/commands/time.js
+++ src/commands/time.js
@@ -1,24 +1,40 @@
+var weather = require("weather-js");
+
 var time = function(bot, sender, args)
 {
-	args[0] = args[0].toUpperCase();
-	if(typeof bot.config.timezones[args[0]] != 'undefined')
+	var arg = args.join(" ");
+
+	var printOutput = function(diff, location)
 	{
-		var zone = bot.config.timezones[args[0]];
-		var diff = zone[1] * 60 * 60 * 1000;
 		var current = new Date();
 		var offset = current.getTimezoneOffset() * 60 * 1000;
 
 		var there = new Date(current.getTime() + offset + diff);
 
-		bot.send(there.toLocaleTimeString() + " - " + zone[0]);
+		bot.send(there.toLocaleTimeString() + " - " + location);
+	}
+
+	if(typeof bot.config.timezones[args[0]] != 'undefined')
+	{
+		var zone = bot.config.timezones[args[0]];
+		var diff = zone[1] * 60 * 60 * 1000;
+		printOutput(diff, zone[0]);
 	}
 	else
 	{
-		bot.send("Unknown time zone, try !timezones");
+		weather.find({search: arg}, function(err, result)
+		{
+			if(err)
+			{
+				bot.send("Error retrieving timezone. Usage !time <location> or !time <timezone>");
+				return;
+			}
+
+			var diff = parseInt(result[0].location.timezone) * 60 * 60 * 1000;
+			var name = result[0].location.name;
+			printOutput(diff, name);
+		});
 	}
 }
 
-module.exports =
-{
-	time: time
-};
+module.exports = { time: time };
