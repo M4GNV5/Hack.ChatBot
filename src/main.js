8224556f9dcce9a7d99932e6d7b067fcb7d85032
foreverwolf@live.be <foreverwolf@live.be>
2015-07-21 19:20:40 +0200

    Added support for ban, ban is no longer case sensitive

--- src/main.js
+++ src/main.js
@@ -4,9 +4,8 @@ var path = require("path");
 var ChatConnection = require("./connection.js");
 var config = require("./config.json");
 
-fs.readdir("./src/commands", function(err, files)
-{
-	if(err)
+fs.readdir("./src/commands", function (err, files) {
+	if (err)
 		throw err;
 
 	var bot = new ChatConnection(config.url, config.nick, config.channel);
@@ -14,25 +13,21 @@ fs.readdir("./src/commands", function(err, files)
 	bot.commands = {};
 	bot.init = [];
 
-	for(var i = 0; i < files.length; i++)
-	{
-		if(path.extname(files[i]) == ".js")
-		{
+	for (var i = 0; i < files.length; i++) {
+		if (path.extname(files[i]) == ".js") {
 			var cmds = require("./commands/" + files[i]);
 
-			if(typeof cmds != 'object')
+			if (typeof cmds != 'object')
 				throw "Invalid command " + files[i];
 
-			if(typeof cmds.init == 'function')
-			{
+			if (typeof cmds.init == 'function') {
 				cmds.init(bot);
 				bot.init.push(cmds.init);
 				delete cmds.init;
 			}
 
-			for(var key in cmds)
-			{
-				if(typeof cmds[key] != 'function')
+			for (var key in cmds) {
+				if (typeof cmds[key] != 'function')
 					throw "Invalid command " + files[i];
 
 				bot.commands[key] = cmds[key];
@@ -40,38 +35,39 @@ fs.readdir("./src/commands", function(err, files)
 		}
 	}
 
-	bot.parseCmd = function(data)
-	{
-		console.log(data.nick + ": " + data.text);
-
-		if(this.bans.indexOf(data.nick) !== -1)
+	bot.parseCmd = function (data) {
+		console.log(this.channel + " | " + data.nick + ": " + data.text);
+	
+		if (data.nick == config.nick)
 			return;
+		
+		if (this.bans.indexOf(data.nick.toLowerCase()) !== -1) {
+			this.send(data.nick + " you are banned.");
+			console.log(this.bans);
+			return;
+		}
 
 		var msg = data.text;
-		if(msg[0] == "!")
-		{
+		if (msg[0] == "!") {
 			var cmd = msg.substr(1).split(" ")[0];
 			var args = msg.substr(2 + cmd.length).split(" ");
-			
-			if(typeof this.commands[cmd] == 'function' && this.commands.hasOwnProperty(cmd))
+
+			if (typeof this.commands[cmd] == 'function' && this.commands.hasOwnProperty(cmd))
 				this.commands[cmd](this, data.nick, args);
 			else
 				this.send("Unknown Command: " + cmd);
 		}
 	}
 
-	bot.on("chat", function(data)
-	{
+	bot.on("chat", function (data) {
 		bot.parseCmd(data);
 	});
 
-	bot.on("info", function(data)
-	{
-		console.log("INFO : " + data.text);
+	bot.on("info", function (data) {
+		console.log(bot.channel + " | INFO : " + data.text);
 	});
 
-	bot.on("warn", function(data)
-	{
-		console.log("WARN : " + data.text);
+	bot.on("warn", function (data) {
+		console.log(bot.channel + " | WARN : " + data.text);
 	});
 });
