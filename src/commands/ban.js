f6ec35e24bb30d61404ab54606c07bbd53e5ac3e
Jakob Löw <eragonjml@googlemail.com>
2015-07-23 18:00:10 +0200

    improved ban system

--- src/commands/ban.js
+++ src/commands/ban.js
@@ -1,16 +1,16 @@
-var init = function(bot) 
+var init = function(bot)
 {
 	bot.bans = [];
 };
 
-var _ban = function(bot, time, user) 
+var _ban = function(bot, time, user)
 {
-	
+
 	bot.bans.push(user);
 
 	if(time > 0)
 	{
-		setTimeout(function() 
+		setTimeout(function()
 		{
 			_unban(bot, user);
 		}, time);
@@ -18,23 +18,32 @@ var _ban = function(bot, time, user)
 }
 var _unban = function(bot, user)
 {
-	
+
 	if (bot.bans.indexOf(user) !== -1)
 		bot.bans.splice(bot.bans.indexOf(user), 1);
 }
 
-var ban = function(bot, sender, args) 
+var ban = function(bot, sender, args)
 {
-	
 	if(bot.requirePerm(sender, "ban"))
 		return;
 
 	var time = parseInt(args[0]) * 1000 || 60000;
 	var bannUser = args.slice(1).join(" ");
-	var nickBan = bannUser.toLowerCase();
-	
-	if(bot.bans.indexOf(nickBan) !== -1) 
+	var nickBan = bannUser.toLowerCase().trim();
+
+	var senderPerm = bot.permLevel[sender] || 0;
+	var banPerm = bot.permLevel[bannUser] || 0;
+	if(senderPerm <= banPerm)
+	{
+		bot.send("@" + sender + " you are not allowed to ban " + bannUser);
+		return;
+	}
+
+	if(bot.bans.indexOf(nickBan) !== -1)
+	{
 		bot.send("@" + sender + " user @" + bannUser + " is already banned");
+	}
 	else
 	{
 		bot.send("User @" + bannUser + " is now banned from using this bot for " + time / 1000 + " seconds");
@@ -42,25 +51,27 @@ var ban = function(bot, sender, args)
 	}
 };
 
-var unban = function(bot, sender, args) 
+var unban = function(bot, sender, args)
 {
-	
+
 	if(bot.requirePerm(sender, "unban"))
 		return;
 
 	var pardonUser = args.join(" ");
 	var nickPardon = pardonUser.toLowerCase();
-	
+
 	if(bot.bans.indexOf(nickPardon) === -1)
+	{
 		bot.send("@" + sender + " user @" + pardonUser + " is not banned yet");
-	else 
+	}
+	else
 	{
 		bot.send("@" + sender + " user @" + pardonUser + " is no longer banned");
 		_unban(bot, nickPardon);
 	}
 };
 
-module.exports = 
+module.exports =
 {
 	init: init,
 	ban: ban,
