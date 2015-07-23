19836ff55b1e04ac816ae53fc662bd650af38e33
Jakob Löw <eragonjml@googlemail.com>
2015-07-23 17:40:40 +0200

    completed verify

--- src/commands/verify.js
+++ src/commands/verify.js
@@ -1,55 +1,89 @@
-var fs = require("fs");
-
-var tripCodes = require("../data/usersTripCode.json");
+var onlineTripcodes = {};
+var init = function(bot)
+{
+	bot.on("chat", function(data)
+	{
+		if(typeof data.trip != 'undefined')
+		{
+			onlineTripcodes[data.nick] = data.trip;
+			if(typeof bot.config.users[data.trip] != 'undefined')
+			{
+				bot.permLevel[data.nick] = bot.config.users[data.trip];
+			}
+		}
+	});
+	bot.on("onlineRemove", function(data)
+	{
+		if(typeof onlineTripcodes[data.nick] != 'undefined')
+			delete onlineTripcodes[data.nick];
+	});
+}
 
 var _verify = function(bot, sender, trip, user)
 {
 	if(user == "vortico")
-			bot.send("$\\color{red}{vortico}$ is allways $\\color{lightgreen}{valid}$!");
-	else if(tripCodes[user])
+			bot.send("$\\color{red}{vortico}$ is always $\\color{lightgreen}{valid}$!");
+	else if(bot.config.tripCodes[user])
 	{
-		if(tripCodes[user] === trip)
+		if(bot.config.tripCodes[user] === trip)
 			bot.send("@" + sender + " " + user + " is $\\color{lightgreen}{valid}$!");
 		else
 			bot.send("@" + sender + " " + user + " is $\\color{red}{not\\space valid}$!");
 	}
 	else
+	{
 		bot.send("@" + sender + " is not on the list, ask one of the admins to add " + user + " .");
+	}
 }
 
-var verify = function(bot, sender, args, data) 
+var verify = function(bot, sender, args, data)
 {
-	if(typeof args[0] != 'undefined' && args[0] == "add" && typeof args[1] != 'undefined')
+	if(typeof args[0] == 'undefined' || args[0].trim() == "") //self verification
 	{
-		if(bot.requirePerm(sender, "verifyAdd"))
-		return;
-	}
-	else if(typeof args[0] != 'undefined' && args[0] == "remove" && typeof args[1] != 'undefined')
-	{
-		if(bot.requirePerm(sender, "verifyAdd"))
-		return;
-	}
-	else if(args[0].length === 0) //Self Verification
-	{
-		if(data.trip)
-			_verify(bot, sender, data.trip, sender);						
+		if(typeof data.trip == 'string')
+			_verify(bot, sender, data.trip, sender);
 		else
 			bot.send("@" + sender + " you do not have a TripCode,\n when logging in use [username]#[password].");
 	}
-	else if(typeof args[1] == 'undefined') //Missusage of syntax
+	else if(args[0] == "add" && typeof args[1] != 'undefined' && typeof args[2] != 'undefined')
+	{
+		if(bot.requirePerm(sender, "verifyAdd"))
+			return;
+
+		bot.config.tripCodes[args[2]] = args[1];
+		bot.send("@" + sender + " added " + args[2] + " to the list");
+	}
+	else if(args[0] == "remove" && typeof args[1] != 'undefined')
+	{
+		if(bot.requirePerm(sender, "verifyAdd"))
+			return;
+
+		if(typeof bot.config.tripCodes[args[1]] == 'undefined')
+		{
+			delete bot.config.tripCodes[args[1]];
+			bot.send("@" + sender + " removed " + args[1] + " from the list");
+		}
+		else
+		{
+			bot.send("@" + sender + " " + args[1] + " is not on the list");
+		}
+	}
+	else if(typeof args[1] == 'undefined')
 	{
 		if(args[0] == "vortico")
-			bot.send("$\\color{red}{vortico}$ is allways $\\color{lightgreen}{valid}$!");
-		else	
-			bot.send("@" + sender + " syntax is !verify [tripCode] [user]");
+			bot.send("$\\color{red}{vortico}$ is always $\\color{lightgreen}{valid}$!");
+		else if(typeof onlineTripcodes[args[0]] != 'undefined')
+			_verify(bot, sender, onlineTripcodes[args[0]], args[0]);
+		else
+			bot.send("@" + sender + " syntax is !verify <user> or !verify <tripcode> <user>");
 	}
 	else //Other Verification
 	{
-		var user = args[1]
+		var user = args[1];
 		if(user.indexOf('@') != -1)
 			user = user.substr(1);
 		_verify(bot, sender, args[0], user);
 	}
 };
 
-module.exports = { verify: verify };
+module.exports = { verify: verify, init: init };
