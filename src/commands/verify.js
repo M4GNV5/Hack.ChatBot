993963633561084cb349521621810ccbb8f25d33
foreverwolf@live.be <foreverwolf@live.be>
2015-07-23 16:39:26 +0200

    Update

--- src/commands/verify.js
+++ src/commands/verify.js
@@ -2,50 +2,53 @@ var fs = require("fs");
 
 var tripCodes = require("../data/usersTripCode.json");
 
+var _verify = function(bot, sender, trip, user)
+{
+	if(user == "vortico")
+			bot.send("$\\color{red}{vortico}$ is allways $\\color{lightgreen}{valid}$!");
+	else if(tripCodes[user])
+	{
+		if(tripCodes[user] === trip)
+			bot.send("@" + sender + " " + user + " is $\\color{lightgreen}{valid}$!");
+		else
+			bot.send("@" + sender + " " + user + " is $\\color{red}{not\\space valid}$!");
+	}
+	else
+		bot.send("@" + sender + " is not on the list, ask one of the admins to add " + user + " .");
+}
+
 var verify = function(bot, sender, args, data) 
 {
 	if(typeof args[0] != 'undefined' && args[0] == "add" && typeof args[1] != 'undefined')
 	{
 		if(bot.requirePerm(sender, "verifyAdd"))
 		return;
-		
-		
 	}
 	else if(typeof args[0] != 'undefined' && args[0] == "remove" && typeof args[1] != 'undefined')
 	{
 		if(bot.requirePerm(sender, "verifyAdd"))
 		return;
-		
-		
 	}
-	else if(args[0].length === 0)
+	else if(args[0].length === 0) //Self Verification
 	{
-		console.log('Test');
-		console.log(args);
-		if(data.trip != 'undefined')
-		{
-			if(tripCodes[sender] === data.trip)
-				bot.send("@" + sender + " is $\\color{lightgreen}{valid}$!");
-			else
-			{
-				bot.send("@" + sender + " is $\\color{red}{not valid}$! Feel free to ask one of the admins to add you to the list");
-			}
-		}
+		if(data.trip)
+			_verify(bot, sender, data.trip, sender);						
+		else
+			bot.send("@" + sender + " you do not have a TripCode,\n when logging in use [username]#[password].");
 	}
-	else if(typeof args[1] == 'undefined')
+	else if(typeof args[1] == 'undefined') //Missusage of syntax
 	{
-		bot.send("@" + sender + " syntax is !verify [user] [tripCode] ");
+		if(args[0] == "vortico")
+			bot.send("$\\color{red}{vortico}$ is allways $\\color{lightgreen}{valid}$!");
+		else	
+			bot.send("@" + sender + " syntax is !verify [tripCode] [user]");
 	}
-	else
+	else //Other Verification
 	{
-		var user = args[0]
+		var user = args[1]
 		if(user.indexOf('@') != -1)
 			user = user.substr(1);
-		
-		if(tripCodes[user] === args[1])
-			bot.send("@" + sender + " " + user + " has been recognized as $\\color{lightgreen}{valid}$!");
-		else
-			bot.send("@" + sender + " " + user + " is $\\color{red}{not valid}$!");
+		_verify(bot, sender, args[0], user);
 	}
 };
 
