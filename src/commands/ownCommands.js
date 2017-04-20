e3d02f272bc1fe62b5656aedda098010ad3312a4
M4GNV5 <jakob@m4gnus.de>
2017-04-20 18:46:09 +0200

    rework !add and !remove into one more-powerfull command

--- src/commands/ownCommands.js
+++ src/commands/ownCommands.js
@@ -1,94 +1,171 @@
-var fs = require("fs");
-
-var init = function(bot)
-{
-	var startup = function()
-	{
-		for(var key in bot.config.ownCommands)
-		{
-			bot.commands[key.toLowerCase()] = createOwnCmdFunc(key, bot.config.ownCommands[key]);
-		}
-	};
-
-	if(!bot.config.ownCommands)
-	{
-		bot.on("config", startup);
-	}
-	else
-	{
-		startup();
-	}
-};
-
-var createOwnCmdFunc = function(cmd, output)
+function createCommand(cmd)
 {
 	return function(bot, sender, args)
 	{
-		var _output = output;
-
-		var replace = function(oldText, newText)
+		if(cmd.type == "text")
 		{
-			if(_output.indexOf(oldText) !== -1)
-				_output = _output.replace(oldText, newText);
-		}
+			var text = cmd.text;
 
-		replace("%url%", bot.url);
-		replace("%channel%", bot.channel);
-		replace("%self%", bot.nick);
-		replace("%sender%", sender);
+			function replace(oldText, newText)
+			{
+				while(text.indexOf(oldText) !== -1)
+					text = text.replace(oldText, newText);
+			}
 
-		for(var i = 0; i < args.length; i++)
-		{
-			if(args[i].trim() != "")
+			replace("%url%", bot.url);
+			replace("%channel%", bot.channel);
+			replace("%self%", bot.nick);
+			replace("%sender%", sender);
+
+			for(var i = 0; i < args.length; i++)
+			{
 				replace("%" + i + "%", args[i]);
+			}
+
+			if(!/%[0-9]+%/g.test(text))
+			{
+				bot.send(text);
+				return;
+			}
 		}
-		if(/%[0-9]+%/g.test(_output))
+		else
 		{
-			bot.send("@" + sender + " not enough arguments!");
+			bot.send("Unknown command type");
 			return;
 		}
 
-
-		bot.send(_output);
+		if(cmd.usage)
+			bot.send("Usage: " + cmd.usage);
+		else
+			bot.send("Failed executing !" + cmd.name);
 	};
+}
+
+exports.init = function(bot)
+{
+	function startup()
+	{
+		for(var key in bot.config.ownCommands)
+		{
+			bot.commands[key] = createCommand(bot.config.ownCommands[key]);
+		}
+	}
+
+	if(!bot.config.ownCommands)
+		bot.on("config", startup);
+	else
+		startup();
 };
 
-var addOwnCmd = function(bot, sender, args)
+exports.command = function(bot, sender, args, data)
 {
-	if(bot.requirePerm(sender, "add"))
+	if(bot.requirePerm(sender, "command"))
 		return;
 
-	if(typeof bot.commands[args[0]] != 'undefined')
+	if(args.length < 2)
+		return bot.send("@" + sender + " Usage: !command set-text|delete|usage|info <cmd> <text ...>");
+
+	var name = args[1].toLowerCase();
+	var text = args.slice(2).join(" ");
+
+	var cmd;
+	if(bot.config.ownCommands.hasOwnProperty(name))
+		cmd = bot.config.ownCommands[name];
+
+	function commandsBy(user)
 	{
-		bot.send("@" + sender + " that commands already exists");
+		/*return bot.config.ownCommands.filter(function(cmd)
+		{
+		return cmd.author.nick == user || cmd.author.trip == user;
+		});*/
+		var commands = [];
+		for(var key in bot.config.ownCommands)
+		{
+			var cmd = bot.config.ownCommands[key];
+			if(cmd.author.nick == user || cmd.author.trip == user)
+				commands.push(cmd);
+		}
+
+		return commands;
 	}
-	else
+	function checkUserPerm(isCreate)
 	{
-		var name = args[0].toLowerCase();
-		var output = args.slice(1).join(" ");
+		var config = bot.config.command;
 
-		bot.commands[name] = createOwnCmdFunc(name, output);
-		bot.config.ownCommands[name] = output;
+		if(!cmd && bot.commands[name])
+			bot.send("@" + sender + " that is a builtin command");
+		else if(!cmd && !isCreate)
+			bot.send("@" + sender + " no such command exists");
+		else if(isCreate && text.trim() == "")
+			bot.send("@" + sender + " command cannot be empty");
+		else if(isCreate && commandsBy(sender).length >= (bot.permLevel[sender] || 0) * config.limitScale + config.limitBase)
+			bot.send("@" + sender + " you already created too many commands");
+		else if(cmd && cmd.author.trip != data.trip && bot.requirePerm(sender, "command-admin", true))
+			bot.send("@" + sender + " cannot modify someone else's command");
+		else
+			return true;
 
-		bot.send("@" + sender + " added command " + name);
+		return false;
+	}
+
+	switch(args[0])
+	{
+		case "set-text":
+			if(!checkUserPerm(true))
+				return;
+
+			bot.config.ownCommands[name] = {
+				name: name,
+				type: "text",
+				author: {trip: data.trip, nick: sender},
+				text: text,
+				created: Date.now(),
+				edited: Date.now()
+			};
+			bot.commands[name] = createCommand(bot.config.ownCommands[name]);
+
+			bot.send("@" + sender + " set command " + name);
+			break;
+
+
+		//TODO set-pastebin
+
+
+		case "delete":
+			if(!checkUserPerm())
+				return;
+
+			delete bot.commands[name];
+			delete bot.config.ownCommands[name];
+
+			bot.send("@" + sender + " deleted command " + name);
+			break;
+
+
+		case "usage":
+			if(!checkUserPerm())
+				return;
+			cmd.usage = text;
+			cmd.edited = Date.now();
+			break;
+
+
+		case "info":
+			if(!checkUserPerm())
+				return;
+
+			var msg = "Info for command !" + name +
+				"\nAuthor: [" + cmd.author.trip + "] " + cmd.author.nick +
+				"\nType: " + cmd.type;
+
+			if(cmd.type == "pastebin")
+				msg += "\nUrl: https://pastebin.com/" + cmd.url;
+			else if(cmd.type == "text")
+				msg += "\nText: " + cmd.text.split("\n")[0].substr(0, 80);
+
+			if(cmd.usage)
+				msg += "\nUsage: " + cmd.usage;
+
+			bot.send(msg);
 	}
 };
-
-var removeOwnCmd = function(bot, sender, args)
-{
-	if(bot.requirePerm(sender, "remove"))
-		return;
-
-	if(typeof bot.commands[args[0]] == 'undefined')
-	{
-		bot.send("@" + sender + " that commands does not exists");
-	}
-	else
-	{
-		delete bot.commands[args[0]];
-		delete bot.config.ownCommands[args[0]];
-		bot.send("@" + sender + " removed command " + args[0]);
-	}
-};
-
-module.exports = {init: init, add: addOwnCmd, remove: removeOwnCmd};
